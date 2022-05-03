b3d01acf7f8a58c4e7d1214ab54762e321a9f0ab
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-03 16:36:28 +0300

    final - added error component

--- user-form/src/App.js
+++ user-form/src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
 
 
 function App() {
+
+  const [userList, setUserList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUserList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 }
--- user-form/src/components/Users/AddUser.js
+++ user-form/src/components/Users/AddUser.js
@@ -2,42 +2,60 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import classes from './AddUser.module.css'
 import Button from "../UI/Button";
+import ErrorModal from "../UI/ErrorModal";
 
-const AddUser = props => {
+const AddUser = (props) => {
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
         if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name and age ( non-empty values )'
+            })
             return;
         }
         if (+enteredAge < 1) {
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid age ( > 0 )'
+            })
             return;
         }
-        // console.log(enteredUsername, enteredAge);
-        setEnteredUsername('')
-        setEnteredAge('')
+        // console.log(enteredUsername, enteredAge); I need to lift this data up so I can transfer it to UserList through App
+        props.onAddUser(enteredUsername, enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     }
     const usernameChangeHandler = (event) => {
         setEnteredUsername(event.target.value);
 
     };
 
-    const ageChangeHandler = (event) =>{
+    const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value);
-    }
+    };
 
+    const errorHandler = () => {
+        setError(null);
+    }
+    
     return (
-        <Card className={classes.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor="username">Username</label>
-                <input id="username" type='text' name="username" value={enteredUsername} onChange={usernameChangeHandler}/>
-                <label htmlFor="age">Age (Years)</label>
-                <input id="age" type='text' name="age" value={enteredAge} onChange={ageChangeHandler}/>
-                <Button type="submit">Add User</Button>
-            </form>
-        </Card>
+        <div>
+            { error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
+            <Card className={classes.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type='text' name="username" value={enteredUsername} onChange={usernameChangeHandler} />
+                    <label htmlFor="age">Age (Years)</label>
+                    <input id="age" type='text' name="age" value={enteredAge} onChange={ageChangeHandler} />
+                    <Button type="submit">Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 };
 
