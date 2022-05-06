5a57cdbdf461ab203a2bf5bd4f3fa594ecdf37b4
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-06 11:21:04 +0300

    load the data on start
    using the useCallback and useEffect to fetch the info on the open the page

--- api requests/src/App.js
+++ api requests/src/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
+
+
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  async function fetchMoviesHandler() {
+  
+
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
 
     try {
       const response = await fetch('https://swapi.dev/api/films/')
-      
+
       if (!response.ok) {
         throw new Error('Something went wrong')
       };
-      
+
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -37,12 +41,16 @@ function App() {
 
     }
     setIsLoading(false);
-  }
+  }, [])
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies.</p>
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies}/>
+    content = <MoviesList movies={movies} />
   }
   if (error) {
     content = <p>{error}</p>
