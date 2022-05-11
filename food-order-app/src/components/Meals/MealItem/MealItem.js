9d34732d1561b4262d8ffa37a95e07b894b10b9d
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-11 21:24:59 +0300

    add cart reducer

--- food-order-app/src/components/Meals/MealItem/MealItem.js
+++ food-order-app/src/components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import classes from './MealItem.module.css';
+import { useContext } from 'react';
 import MealItemForm from './MealItemForm';
+import classes from './MealItem.module.css';
+import CartContext from '../../../store/cart-context';
+
 
 const MealItem = (props) => {
+    const cartCtx = useContext(CartContext)
     const price = `€${props.price.toFixed(2)}`
 
+    const addToCartHandler = (amount) => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    }
     return (
         <li className={classes.meal}>
             <div>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id} />
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     );
