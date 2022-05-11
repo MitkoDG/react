4d85817219f287acddad2e73c5c32d6cec4fe487
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-11 20:10:47 +0300

    cart update logic

--- food-order-app/src/store/CartProvider.js
+++ food-order-app/src/store/CartProvider.js
@@ -1,13 +1,40 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
 
-const CartPrivider = (props) =>{
-    const addItemToCartHandler = (item) => {};
+const defaultCartState ={
+    items: [],
+    totalAmount: 0
+};
 
-    const removeItemFromCartHandler = (id) =>{};
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updateItems = state.items.concat(action.item);
+        const updateTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        return {
+            items: updateItems,
+            totalAmount: updateTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+const CartPrivider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
+     };
+
+    const removeItemFromCartHandler = (id) => { 
+        dispatchCartAction({type: 'REMOVE', id: id});
+
+    };
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     }
