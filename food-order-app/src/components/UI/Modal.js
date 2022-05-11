41a040c8346dfb878f7d7e6a702f7a1ed5f85840
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-11 16:35:10 +0300

    managing Cart
    be able to open the cart (with dummy info) and close it

--- food-order-app/src/components/UI/Modal.js
+++ food-order-app/src/components/UI/Modal.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 
 const Backdrop = (props) => {
     return(
-        <div className={classes.backdrop} />
+        <div className={classes.backdrop} onClick={props.onClose}/>
     )
 };
 
@@ -20,7 +20,7 @@ const portalElement = document.getElementById('overlays')
 const Modal = (props) => {
     return(
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </Fragment>
     )
