2a50e108ed9f1177df6fec1d73ed07aca66c4511
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-15 16:40:40 +0300

    button disabled
    ... when form is not valid

--- forms-user-input/src/components/SimpleInput.js
+++ forms-user-input/src/components/SimpleInput.js
@@ -8,6 +8,11 @@ const SimpleInput = (props) => {
   const enteredNameIsValid = enteredName.trim() !== '';
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
 
+  let formIsValid = false;
+
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -50,7 +55,7 @@ const SimpleInput = (props) => {
       </div>
       {nameInputIsInvalid && <p className='error-text'>Name must not be empty !</p>}
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
