323962cf9437bb504f734cfe93a4018f3a490208
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-05-24 15:57:45 +0300

    reusing the fetch code for login as well

--- authentication/src/components/Auth/AuthForm.js
+++ authentication/src/components/Auth/AuthForm.js
@@ -2,8 +2,6 @@ import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
-const registerUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEbQ9OikGSxjdjcBfjG8iUDLKaKNLX0KA'
-
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false)
@@ -22,34 +20,42 @@ const AuthForm = () => {
     const enteredPassword = passwordInputRef.current.value;
 
     setIsLoading(true)
-    if (isLogin) {
+    let url;
 
+    if (isLogin) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDEbQ9OikGSxjdjcBfjG8iUDLKaKNLX0KA'
     } else {
-      fetch(registerUrl, {
-        method: 'POST',
-        body: JSON.stringify({
-          email: enteredEmail,
-          password: enteredPassword,
-          returnSecureToken: true
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(res=>{
-        setIsLoading(false)
-        if (res.ok) {
-          
-        } else{
-          return res.json().then(data => {
-            let errorMsg = 'Authentication failed';
-            if (data && data.error && data.error.message) {
-              errorMsg = data.error.message;
-            }
-            setHasError(errorMsg)
-          })
-        }
-      })
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEbQ9OikGSxjdjcBfjG8iUDLKaKNLX0KA'
     }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res=>{
+      setIsLoading(false)
+      if (res.ok) {
+        return res.json()        
+      } else{
+        return res.json().then(data => {
+          let errorMsg = 'Authentication failed';
+          if (data && data.error && data.error.message) {
+            errorMsg = data.error.message;
+          }
+          // setHasError(errorMsg)
+          throw new Error(errorMsg) // this is forward the error to the catch block thats why we dont need the prev line
+        })
+      }
+    }).then(data => {
+      console.log(data);
+    }).catch((err) =>{
+      setHasError(err.message)
+    })
     setHasError(false);
   }
   
