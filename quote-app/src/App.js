5f154c95c4f2679702e4cd83244cc304ef7975e4
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-11 16:33:22 +0300

    adding navigation components

--- quote-app/src/App.js
+++ quote-app/src/App.js
@@ -1,24 +1,27 @@
 import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetails from "./pages/QuoteDetails";
 
 function App() {
   return (
-    <Switch>
-      <Route path='/' exact>
-        <Redirect to='/quotes' />
-      </Route>
-      <Route path='/quotes' exact>
-        <AllQuotes />
-      </Route>
-      <Route path='/quotes/:quoteId'>
-        <QuoteDetails />
-      </Route>
-      <Route path='/new-quote'>
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetails />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
