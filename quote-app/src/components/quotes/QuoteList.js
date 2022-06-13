38a5cc455621d5ce3d3a05482e1b6a20dbe5cd5f
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-13 16:55:13 +0300

    adding btn for sorting

--- quote-app/src/components/quotes/QuoteList.js
+++ quote-app/src/components/quotes/QuoteList.js
@@ -1,11 +1,24 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
 const QuoteList = (props) => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search)
+
+  const changeSortingHandler = () =>{
+    history.push('/quotes?sort=asc');
+  };
+
   return (
     <Fragment>
+      <div className={classes.sorting}>
+        <button onClick={changeSortingHandler}>Sort Ascending</button>
+      </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
           <QuoteItem
