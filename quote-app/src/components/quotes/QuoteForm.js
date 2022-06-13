37cf404d4215db7fab77a78d5d05eb305bc5b6f5
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-13 13:05:19 +0300

    adding Prompt for leaving the form

--- quote-app/src/components/quotes/QuoteForm.js
+++ quote-app/src/components/quotes/QuoteForm.js
@@ -1,10 +1,13 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import Card from '../UI/Card';
 import LoadingSpinner from '../UI/LoadingSpinner';
 import classes from './QuoteForm.module.css';
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
+
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -19,9 +22,19 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
+  const finishEnteringHandler = () =>{
+    setIsEntering(false);
+  }
+
+  const formFocusedHandler = () =>{
+    setIsEntering(true);
+  };
+
   return (
+    <Fragment>
+      <Prompt when={isEntering} message={(location) => "Are you sure you want to leave ? All your entered data will be lost!"} />
     <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
+      <form onFocus={formFocusedHandler} className={classes.form} onSubmit={submitFormHandler}>
         {props.isLoading && (
           <div className={classes.loading}>
             <LoadingSpinner />
@@ -37,10 +50,11 @@ const QuoteForm = (props) => {
           <textarea id='text' rows='5' ref={textInputRef}></textarea>
         </div>
         <div className={classes.actions}>
-          <button className='btn'>Add Quote</button>
+          <button onClick={finishEnteringHandler} className='btn'>Add Quote</button>
         </div>
       </form>
     </Card>
+    </Fragment>
   );
 };
 
