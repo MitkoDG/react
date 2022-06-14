6e3ca3c03aa68fcafa8d83404a0fe56120b0366f
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-14 16:42:57 +0300

    nested Route for comments

--- quote-app/src/pages/QuoteDetails.js
+++ quote-app/src/pages/QuoteDetails.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { Route } from "react-router-dom";
+import { Link, Route } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
@@ -22,7 +22,14 @@ const QuoteDetails = () => {
     return (
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author}/>
-            <Route path='/quotes/:quoteId/comments'>
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
         </Fragment>
