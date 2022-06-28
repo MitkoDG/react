85af2d089e5e9b238e2904e5cb05b0b4ce712ec2
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-06-28 13:33:05 +0300

    useReducer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,10 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import CharacterList from './components/CharacterList'
+import { Counter } from './components/Reducer';
 
 function App() {
     const [name, setName] = useState('');
     const [count, setCount] = useState(0);
+    const enteredInputName = useRef()
 
     useEffect(() => {
       setTimeout(() => {
@@ -12,14 +14,24 @@ function App() {
       }, 1500);
     }, []);
 
+    function changeNameFunc(name) {
+      const newName = enteredInputName.current.value
+      setName(newName)
+    }
+
     return (
       <div className="App">
+        <input placeholder="name" ref={enteredInputName}/>
+        <button onClick={changeNameFunc}> Change the name</button>
         <h2>{!name ? 'Loading...' : name}</h2>
+        <Counter />
         <h3>{count}</h3>
         <button onClick={() => setCount(x => x + 1)}>+</button>
         <CharacterList />
+        
       </div>
     );
+    
   }
 
 export default App;
