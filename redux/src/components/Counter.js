de352cec1439fb3de78d52c797a19d8092f4a09d
MitkoDG <83611831+MitkoDG@users.noreply.github.com>
2022-08-03 21:21:06 +0300

    adding action value

--- redux/src/components/Counter.js
+++ redux/src/components/Counter.js
@@ -9,6 +9,11 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch( {type: 'increment'})
   }
+
+  const increaseHandler = () => {
+    dispatch( {type: 'increase', value: 10})
+  }
+
   const decrementHandler = () => {
     dispatch( {type: 'decrement'})
   }
@@ -20,6 +25,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 10</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
