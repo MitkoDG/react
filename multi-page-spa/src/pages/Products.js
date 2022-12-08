bd8d5a8c2a802949b93588e75d933bf820839b65
DimitarDG <83611831+MitkoDG@users.noreply.github.com>
2022-12-08 15:21:01 +0200

    Update Products.js

--- multi-page-spa/src/pages/Products.js
+++ multi-page-spa/src/pages/Products.js
@@ -10,10 +10,10 @@ const Products = () => {
                     <Link to='/products/p1'>A Book</Link>
                 </li>
                 <li>
-                <Link to='/products/p2'>A Carpet</Link>
+                    <Link to='/products/p2'>A Carpet</Link>
                 </li>
                 <li>
-                <Link to='/products/p3'>An Online Course</Link>
+                    <Link to='/products/p3'>An Online Course</Link>
                 </li>
             </ul>
         </section>
